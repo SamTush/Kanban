694caab932e89869797f38607148e86cee044548
Gracerue <munyaradzi045@gmail.com>
2023-03-20 15:33:46 +0200

    Add webpack and jest

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, 'src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+          {
+            test: /\.sass$/,
+            use: [
+              'style-loader',
+              'sass-loader',
+              'css-loader'
+            ]
+          }
+        ]
+      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Kanban',
+            filename: 'index.html',
+            template: 'src/template.html',
+        })
+    ]
+}
