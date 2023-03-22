80fe54c34676a0bb62c5470b6a4b8d4cb97c453e
Gracerue <munyaradzi045@gmail.com>
2023-03-22 18:20:32 +0200

    Fix like button

--- src/modules/display.js
+++ src/modules/display.js
@@ -7,7 +7,7 @@ const layout = (movie) => {
       <img src="${movie.image.original}" alt="" >
       <span>
       <h2>Arrow Season ${movie.number}</h2>
-      <button type="button" class="like-btn"><i class="fa-sharp fa-solid fa-square-heart"></i></button>
+      <button type="button" class="like-btn"><i class="fa-solid fa-heart btn btn-secondary btn-sm  text-xl font-bold"></i></button>
       <span class="likes"> 0 likes </span>
       </span>     
   </div>
